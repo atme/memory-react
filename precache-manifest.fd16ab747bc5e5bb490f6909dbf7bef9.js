ee123802281613f8f7ebfa551add65964229de0b
Serge Pirogov <d3ljke@gmail.com>
2019-02-15 15:59:26 +0100

    Updates

---
+++ precache-manifest.fd16ab747bc5e5bb490f6909dbf7bef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "736569c015d0f82c1866",
+    "url": "/memory-react/static/css/main.fee8e8e1.chunk.css"
+  },
+  {
+    "revision": "736569c015d0f82c1866",
+    "url": "/memory-react/static/js/main.736569c0.chunk.js"
+  },
+  {
+    "revision": "db5bfe67f6cdf2914cec",
+    "url": "/memory-react/static/js/1.db5bfe67.chunk.js"
+  },
+  {
+    "revision": "98abbd804fbf0ae501f6",
+    "url": "/memory-react/static/js/runtime~main.98abbd80.js"
+  },
+  {
+    "revision": "bae187973543b0d88d39d5476e9e3aba",
+    "url": "/memory-react/index.html"
+  }
+];
