41f0dc084f553d8bfb9fbaa9b9edee7503c4d182
Serge Pirogov <d3ljke@gmail.com>
2019-06-06 23:23:39 +0200

    Updates

---
+++ precache-manifest.bbc14e1ca2aef214bc84aeaa87e1877c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87404546943cbc374bd4a22e5ba1cdd",
+    "url": "/memory-react/index.html"
+  },
+  {
+    "revision": "ebd64370efeb0a3a23ed",
+    "url": "/memory-react/static/css/main.6434fed7.chunk.css"
+  },
+  {
+    "revision": "1f7c1e3971d5bb19a3ae",
+    "url": "/memory-react/static/js/2.90773b9e.chunk.js"
+  },
+  {
+    "revision": "ebd64370efeb0a3a23ed",
+    "url": "/memory-react/static/js/main.2e3221d4.chunk.js"
+  },
+  {
+    "revision": "39a3745a50e89c0b0ea4",
+    "url": "/memory-react/static/js/runtime~main.7c494fe2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memory-react/precache-manifest.195fa00dc1897fc7e16ed0aa3c1b85c5.js"
+  "/memory-react/precache-manifest.bbc14e1ca2aef214bc84aeaa87e1877c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/memory-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memory-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
